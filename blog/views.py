da48ab515706b0eba48ea2ccebaec21f827685b2
usbpendrive <ferry.fernando@gmail.com>
2025-12-09 09:40:56 +0700

    Add page pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
+from django.core.paginator import EmptyPage, Paginator
 from django.shortcuts import get_object_or_404, render
+
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
-        request, 'blog/post/post_list.html',
+        request, 'blog/post/list.html',
         {'posts': posts},
     )
 
