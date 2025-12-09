329bc679026fdcaa9a072ee4c5b7cd43aff80165
usbpendrive <ferry.fernando@gmail.com>
2025-12-09 10:52:59 +0700

    Add comment system to blog post

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 
 
 class PostListView(ListView):
@@ -37,10 +38,16 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post},
+        {
+            'post': post,
+            'comments': comments,
+            'form': form,
+        },
     )
 
 
@@ -87,3 +94,23 @@ def post_share(request, post_id):
         },
     )
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        return render(
+            request,
+            'blog/post/comment.html',
+            {
+                'post': post,
+                'form': form,
+                'comment': comment,
+            }
+        )
