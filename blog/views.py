b64834b1a3144869a96fc965d869fdc7d676f333
usbpendrive <ferry.fernando@gmail.com>
2025-12-09 12:48:33 +0700

    Applied trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib.postgres.search import (
-    SearchVector, SearchQuery, SearchRank
+    TrigramSimilarity, SearchVector, SearchQuery, SearchRank
 )
 from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
@@ -146,16 +146,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = (SearchVector(
-                'title', weight='A') +
-                             SearchVector('body', weight='B'))
-            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query)
+                    similarity=TrigramSimilarity('title', query)
                 )
-                .filter(rank__gte=0.3).order_by('-rank'))
+                .filter(similarity__gt=0.1).order_by('-similarity'))
     return render(
         request,
         'blog/post/search.html',
