adc36815cfd42dd0ca787cdcfa3de446585d7e72
usbpendrive <ferry.fernando@gmail.com>
2025-12-09 11:27:17 +0700

    Similar posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -49,6 +50,13 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     return render(
         request,
         'blog/post/detail.html',
@@ -56,6 +64,7 @@ def post_detail(request, year, month, day, post):
             'post': post,
             'comments': comments,
             'form': form,
+            'similar_posts': similar_posts,
         },
     )
 
