62be0d144ad1828cb918caf55a6e2a17afaee86f
usbpendrive <ferry.fernando@gmail.com>
2025-12-09 12:44:15 +0700

    Add weighted search

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector, SearchQuery, SearchRank
+)
 from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
@@ -144,11 +146,16 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = (SearchVector(
+                'title', weight='A') +
+                             SearchVector('body', weight='B'))
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body'),
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
                 )
-                .filter(search=query))
+                .filter(rank__gte=0.3).order_by('-rank'))
     return render(
         request,
         'blog/post/search.html',
